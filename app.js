c7cc5b9f175f309568c2f4f5bb56e23c95032f0b
varinea <varinea.lopez@gmail.com>
2020-04-12 01:24:33 -0400

    primer commit

---
+++ app.js
@@ -0,0 +1,15 @@
+
+/* Demo purposes only */
+$(".hover").mouseleave(
+  function () {
+    $(this).removeClass("hover");
+  }
+);
+
+
+/* Demo purposes only */
+$(".hover").mouseleave(
+  function () {
+    $(this).removeClass("hover");
+  }
+); 
