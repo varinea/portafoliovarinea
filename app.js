47e8704603e6b19075206a6e5c88134841ca66b4
varinea <varinea.lopez@gmail.com>
2020-05-04 06:54:49 -0400

    fixed responsive

--- app.js
+++ app.js
@@ -1,18 +1,3 @@
-let vari = "varinea";
-console.log("vari");
-
-/* Demo purposes only */
-$(".hover").mouseleave(
-    function () {
-      $(this).removeClass("hover");
-    }
-  );
-  
-
-
-  /* Demo purposes only */
-$(".hover").mouseleave(
-  function () {
-    $(this).removeClass("hover");
-  }
-);
+function myFunction() {
+  document.getElementById("demo");
+}
